667e50fecbffe4e564b9353a858a06009f07eafe
zoreacry <75418836+zoreacry@users.noreply.github.com>
2022-04-16 09:33:29 +0800

    Add files via upload

---
+++ Admittance_control/Initial motion parameters/Ini_motn_para.m
@@ -0,0 +1,12 @@
+function [T_s,m,dx_re,dx_d_re,dx_dd_re] = Ini_motn_para()
+
+
+T_s=0.01;                    %s
+m=5;                        %kg
+
+dx_re=[0;0;0;0;0;0];        %m            
+dx_d_re=[0;0;0;0;0;0];      %m/s
+dx_dd_re=[0;0;0;0;0;0];     %m/s'2
+
+end
+
